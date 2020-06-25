033bef76bfb51b754c06a0780d919cfa0f7b89d7
dongwkim <dotorry@gmail.com>
2020-06-25 07:14:11 +0000

    add unit field

--- query.py
+++ query.py
@@ -7,6 +7,9 @@ import time
 from random import randint
 from pymongo import ReadPreference
 
+"""
+    단순 조회 - 전체 데이터 리턴 
+"""
 def query(coll,groupnm,tagnm):
     query = {
                     'tag_group_nm': groupnm, 
@@ -16,7 +19,6 @@ def query(coll,groupnm,tagnm):
                     }
             }
     start = time.time()
-    #print(pipeline)
     result = list(coll.find(query))
     end = time.time()
     elapse = end - start
@@ -25,6 +27,10 @@ def query(coll,groupnm,tagnm):
     #print(result)
     qlen=''
     return elapse,qlen
+
+"""
+    Aggregation - 14일 이내 데이터 조회시
+"""
 def query_agg(coll,groupnm,tagnm,stime,etime):
     pipeline = [
             {
@@ -45,7 +51,7 @@ def query_agg(coll,groupnm,tagnm,stime,etime):
             }, {
                 '$bucketAuto': {
                     'groupBy': '$tag.ts', 
-                    'buckets': 2880, 
+                    'buckets': bsz, 
                     'output': {
                         'tag_group_nm': {
                             '$min': '$tag_group_nm'
@@ -61,6 +67,9 @@ def query_agg(coll,groupnm,tagnm,stime,etime):
                         }, 
                         'avg': {
                             '$avg': '$tag.val'
+                        },
+                        'unit' :{
+                            '$first':'$unit'
                         }
                     }
                 }
@@ -72,25 +81,38 @@ def query_agg(coll,groupnm,tagnm,stime,etime):
                             'ts': '$ts', 
                             'val': '$first'
                         }
+                    },
+                    'unit':{
+                        '$first': '$unit'
                     }
                 }
-            },
-            {
-                '$sort':{
-                    'trend.ts':1
+            },{
+                '$project':{
+                    '_id':0,
+                    "tag_nm":'$_id',
+                    'trend':1,
+                    'unit' :1
                 }
-            }
+            },
+            #  {
+            #     '$sort':{
+            #         'trend.ts':1
+            #     }
+            # }
         ]
     start = time.time()
-    #print(pipeline)
     result = list(coll.aggregate(pipeline))
     end = time.time()
     elapse = end - start
-    # pprint.pprint(result)
     qlen = len(result[0]['trend'])
-    print("tag: {},interval {}".format(result[0]['_id'],result[0]['trend'][1]['ts'] -  result[0]['trend'][0]['ts']))
-    return elapse,qlen
+    print("tag: {},interval {}".format(result[0]['tag_nm'],result[0]['trend'][1]['ts'] -  result[0]['trend'][0]['ts']))
+    # 소요시간, 반환되는 어레이 길이, 결과값 
+    return elapse,qlen,result
 
+"""
+    Aggregation - 14일 이상 데이터 조회시
+    Inner Array의 값을 버케팅 하지 않고 Root Document의 Computed 값을 사용
+"""
 def query_agg_10min(coll,groupnm,tagnm,stime,etime):
     pipeline = [
             {
@@ -107,7 +129,7 @@ def query_agg_10min(coll,groupnm,tagnm,stime,etime):
             },{
                 '$bucketAuto': {
                     'groupBy': '$last', 
-                    'buckets': 2880, 
+                    'buckets': bsz, 
                     'output': {
                         'tag_group_nm': {
                             '$min': '$tag_group_nm'
@@ -120,6 +142,9 @@ def query_agg_10min(coll,groupnm,tagnm,stime,etime):
                         }, 
                         'val': {
                             '$first': '$val'
+                        },
+                        'unit' :{
+                            '$first':'$unit'
                         }
                     }
                 }
@@ -131,9 +156,20 @@ def query_agg_10min(coll,groupnm,tagnm,stime,etime):
                             'ts': '$ts', 
                             'val': '$val'
                         }
+                    },
+                    'unit':{
+                        '$first': '$unit'
+                         
                     }
                 }
-            },
+            },{
+                '$project':{
+                    '_id':0,
+                    "tag_nm":'$_id',
+                    'trend':1,
+                    'unit':1
+                }
+            }
             # {
             #     '$sort':{
             #         'trend.ts':1
@@ -141,24 +177,28 @@ def query_agg_10min(coll,groupnm,tagnm,stime,etime):
             # }
         ]
     start = time.time()
-    #print(pipeline)
     result = list(coll.aggregate(pipeline))
     end = time.time()
     elapse = end - start
-    # pprint.pprint(result)
     qlen = len(result[0]['trend'])
-    print("tag: {},interval {}".format(result[0]['_id'],(result[0]['trend'][1]['ts'] -  result[0]['trend'][0]['ts']).total_seconds()))
-    return elapse,qlen
+    print("tag: {},interval {}".format(result[0]['tag_nm'],(result[0]['trend'][1]['ts'] -  result[0]['trend'][0]['ts']).total_seconds()))
+    # 소요시간, 반환되는 어레이 길이, 결과값 
+    return elapse,qlen,result
 
-def query_agg_facet(coll,groupnm,tagnm):
+'''
+def query_agg_facet(coll,groupnm):
     pipeline = [
     {
-        '$match': {
-            'tag_group_nm': groupnm, 
-            'first': {
-                '$gte': datetime(2020, 5, 1, 0, 0, 0, tzinfo=timezone.utc)
-            }
-        }
+                '$match': {
+                    'tag_group_nm': groupnm, 
+                    # 'tag_nm': tagnm, 
+                    'first': {
+                        '$gte': stime
+                    },
+                    'last': {
+                        '$lt': etime
+                    }
+                }
     }, {
         '$unwind': {
             'path': '$tag'
@@ -173,7 +213,7 @@ def query_agg_facet(coll,groupnm,tagnm):
                 }, {
                     '$bucketAuto': {
                         'groupBy': '$tag.ts', 
-                        'buckets': 2000, 
+                        'buckets': 2880, 
                         'output': {
                             'tag_group_nm': {
                                 '$min': '$tag_group_nm'
@@ -202,7 +242,7 @@ def query_agg_facet(coll,groupnm,tagnm):
                 }, {
                     '$bucketAuto': {
                         'groupBy': '$tag.ts', 
-                        'buckets': 2000, 
+                        'buckets': 2880, 
                         'output': {
                             'tag_group_nm': {
                                 '$min': '$tag_group_nm'
@@ -231,7 +271,7 @@ def query_agg_facet(coll,groupnm,tagnm):
                 }, {
                     '$bucketAuto': {
                         'groupBy': '$tag.ts', 
-                        'buckets': 2000, 
+                        'buckets': 2880, 
                         'output': {
                             'tag_group_nm': {
                                 '$min': '$tag_group_nm'
@@ -264,33 +304,31 @@ def query_agg_facet(coll,groupnm,tagnm):
     qlen = len(result[0]['tag0'])
     print("tag: {},interval {}".format('tag0',result[0]['tag0'][1]['ts'] -  result[0]['tag0'][0]['ts']))
     return elapse,qlen
+'''
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', type=str, default="iot", help="MongoDB Database Name")
     parser.add_argument('-c', type=str, default="type2", help="MongoDB Database Collection")
-    parser.add_argument('-t', type=int, default=1, help="Thread Count")
-    parser.add_argument('-y', type=int, default=1, help="Delay")
-    parser.add_argument('-w', type=int, default=10000, help="Write Count")
     parser.add_argument('-uri', type=str, default="mongodb+srv://admin:welcomemongo@trend.r8dos.mongodb.net/iot?readPreference=secondary",help="MongoDB uri")
+    parser.add_argument('-repl', type=str, default="atlas-jay9vh-shard-0",help="ReplicaSet")
     args = parser.parse_args()
     uri = args.uri
     dbname = args.d
     colname = args.c
-    delay = args.y
-    tc = args.t # Thread Count
-    target_count = args.w
+    replset = args.repl
     
-    client = MongoClient(uri,replicaset='atlas-jay9vh-shard-0',readPreference='secondaryPreferred')
+    client = MongoClient(uri,replicaset=replset,readPreference='secondaryPreferred')
     # client = MongoClient(uri,replicaset='atlas-jay9vh-shard-0',readPreference='primary')
     db = client[dbname]
     coll=db.get_collection(colname)
 
     result =[]
     total_elapsed =0
+    output =[]
     cnt =0
-    endTime =datetime(2020, 5, 31, 23, 59, 0, tzinfo=timezone.utc)
-    startTime =datetime(2020, 2,1, 0, 0, 0, tzinfo=timezone.utc)
+    
+    
 
     endTimeEpoch = endTime.timestamp()
     startnTimeEpoch = startTime.timestamp()
@@ -311,11 +349,17 @@ def main():
             else:
                 result.append(query_agg_10min(coll,groupnm=group,tagnm=tag,stime=startTime,etime=endTime))
         elapsed = sum([pair[0] for pair in result])
+
+        # 쿼리 검증
+        if showoutput is True:
+            output.append([pair[2][0] for pair in result])
+            print(output[0])
         total_elapsed += elapsed
         avg_elapsed = total_elapsed/cnt
-        print("{:10s} : elapsed :{:5f} : avg elapsed :{:5f}, detail{}".format(group,elapsed,avg_elapsed,result))
+        print("{:10s} : elapsed :{:5f} : avg elapsed :{:5f}, detail{}".format(group,elapsed,avg_elapsed,[pair[0:2] for pair in result]))
         result = []
-        time.sleep(0)
+        output=[]
+        time.sleep(5)
     """
     while True:
         group = 'group' + str(randint(0,10))
@@ -326,4 +370,13 @@ def main():
 
 
 if __name__ == '__main__':
+
+    # 쿼리 결과 조회여부
+    showoutput = True
+    # bucketSize
+    bsz=2880
+
+    startTime =datetime(2020, 5,1, 0, 0, 0, tzinfo=timezone.utc)
+    endTime =datetime(2020, 5, 31, 23, 59, 0, tzinfo=timezone.utc)
+
     main()
